8c06b8008e345da61e1c070ce08dd5bb6fcf258b
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-11-12 18:02:54 +0530

    Methods of list is added

---
+++ 14)List_of_Methods.py
@@ -0,0 +1,24 @@
+Names_of_Friends = ["Nikiita", "Shruri", "Prany", "Rani","Rushab", "Sonal","Rewati","Akansha"]
+
+Names_of_Friends.append("Sujata") #append method to add a string at the end of the list
+
+print(Names_of_Friends)
+
+Numbers = [1 , 2 , 5 , 8 , 65 , 90, 23 , 45 , 67]
+Numbers.sort()
+print(Numbers)
+
+Numbers.reverse()
+print(Numbers)
+
+
+Numbers.insert(2,66)
+print(Numbers)
+
+Numbers.pop(5)
+print(Numbers)
+
+Numbers.remove(1)
+print(Numbers)
+
+
