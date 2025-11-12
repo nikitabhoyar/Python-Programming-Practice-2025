93c40e9b233ff2025e8b1047f46c7d84889408ae
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-11-12 17:33:07 +0530

    List program in python which is mutuable

---
+++ 13)List_Program.py
@@ -0,0 +1,8 @@
+
+Names_of_Friends = ["Nikiita", "Shruri", "Prany", "Rani","Rushab", "Sonal","Rewati","Akansha"]
+
+print(Names_of_Friends)
+
+print(Names_of_Friends[4])
+
+print(Names_of_Friends[1:4])
