ac4cea461d4824f49d3408ba315dae550f5c97e8
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-11-12 16:46:03 +0530

    Escape sequences program is added

---
+++ 11)Escape_sequence_Program.py
@@ -0,0 +1,29 @@
+string = "Nikita will become DevOps engineer\n no she will be \nPython Developer"
+print(string)
+
+sentence = "AWS DevOps Engineer\tIgnition Engineer"
+print(sentence)
+
+single_quote = "She is a \"Professional\""
+print(single_quote)
+
+carriage_return = "Hello\rWorld"
+print(carriage_return)
+
+Backspace = "Hello\bWorld"
+print(Backspace)
+
+Form_feed = "Hello\fWorld"
+print(Form_feed)
+
+Vertical_tab = "Heelo\vWorld"
+print(Vertical_tab)
+
+print("\a")
+
+Null_Character = "Hello\0World"
+print(Null_Character)
+
+Hex_Value = "\x48\x69"
+print(Hex_Value)
+
