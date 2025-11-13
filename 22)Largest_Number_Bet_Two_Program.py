9fe119bf2a73c099b652cc86e2ffcd055034cd46
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-11-13 20:16:14 +0530

    Largest number between 2 numbers by if else

---
+++ 22)Largest_Number_Bet_Two_Program.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the First Number:"))
+
+num2 = int(input("Enter the Second Number:"))
+
+if num1>num2:
+    print("Num1 is the largest number")
+else:
+    print("Num2 is the largest number")
