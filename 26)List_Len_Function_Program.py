d142e280b228ac6c53d78b815da6bd7380defc9a
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-10 05:28:19 +0530

    Len funation is added

---
+++ 26)List_Len_Function_Program.py
@@ -0,0 +1,6 @@
+thislist = ["apple","banana","grapes","custurdapple","mango","oranges"]
+print(len(thislist))
+
+names = ["nikita","prashant","prachi","ranu","kanak","sahil","nikhil","rani"]
+print(len(names))
+
