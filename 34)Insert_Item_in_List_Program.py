fb747f98f42c24d8ff43f997580053d4a885efe3
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-10 19:31:19 +0530

    Inserting new item value in list by insert() method

---
+++ 34)Insert_Item_in_List_Program.py
@@ -0,0 +1,14 @@
+#Insert Items
+#To insert a new list item, without replacing any of the existing values,
+#we can use the insert() method.
+
+#The insert() method inserts an item at the specified index:
+
+thislist = ["apple","banana","cherry","orange"]
+thislist.insert(2,"watermelon")
+print(thislist)
+
+names = ["nikita","prashant","rani","akshay","avinash","priya"]
+names.insert(3,"Nikhil")
+print(names)
+
