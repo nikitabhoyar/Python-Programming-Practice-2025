d241b247fd9c88544cfa49b114eee34a29a32a2f
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-10 07:49:52 +0530

    Range of index program is added

---
+++ 32)Range_of_iindex_list_Program.py
@@ -0,0 +1,10 @@
+
+thislist = ["apple","mangoes","grapes","avacado","strawberry","cherry"]
+print(thislist[2:5])
+
+names = ["Nikita","Nikhil","Prachi","Aditya","Varsha mommy","Papa","Shanshak"]
+print(names[1:3])
+
+flowers = ["lily","jasmine","rose","sunflower"]
+print(flowers[2:4])
+
