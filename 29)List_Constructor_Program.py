0737b501c0df5b35cf99a4f6e2784db4a519cc3f
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-10 06:38:17 +0530

    list constructor program is added

---
+++ 29)List_Constructor_Program.py
@@ -0,0 +1,10 @@
+#list()contructor to make list
+
+thislist = list(("apple","banana","orange","grapes","grapes","mango"))
+print(thislist)
+
+names = list(("Aparna","Nikita","Divya","Prachi","Akansha","Shashank","Nikhil","Sweety"))
+print(names)
+print(len(names))
+print(type(names))
+
