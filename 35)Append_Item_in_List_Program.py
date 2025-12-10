f4e81c2eb372a93dd3ce776317fefe225feea99a
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-10 19:48:33 +0530

    Append method program is added

---
+++ 35)Append_Item_in_List_Program.py
@@ -0,0 +1,13 @@
+#Append Items
+#To add an item to the end of the list, use the append() method:
+
+thislist = ["apple","banana","cherry","orange","grapes"]
+thislist.append("watermelon")
+print(thislist)
+
+Names = ["Nikita","Priya","Shruti","Ashish","Prajwal","Saurab"]
+Names.append("Nikhil")
+print(Names)
+
+
+
