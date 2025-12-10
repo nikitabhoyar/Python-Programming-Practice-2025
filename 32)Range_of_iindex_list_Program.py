00b37db18c07a7f1d4018d483a2a5e8dda08e520
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-10 07:57:40 +0530

    program is updated

--- 32)Range_of_iindex_list_Program.py
+++ 32)Range_of_iindex_list_Program.py
@@ -8,3 +8,8 @@ print(names[1:3])
 flowers = ["lily","jasmine","rose","sunflower"]
 print(flowers[2:4])
 
+
+# By leaving out the start value, the range will start at the first item:
+
+thislist = ["apple","banana","cherry","orange","kiwi","kiwi","melon","mangoes"]
+print(thislist[:4])
