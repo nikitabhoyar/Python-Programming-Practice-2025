0ef5e20a7691732d900b80674025889d2b598b4d
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-11 17:07:15 +0530

    Deleting the specific index by pop method

---
+++ 38)Pop_Method_List_Program.py
@@ -0,0 +1,35 @@
+#Remove specified index.abs
+
+#The pop() method removes the specified index 
+
+#Example 
+
+#Remove the second item.
+
+thislist = ["apple","banana","cherry"]
+thislist.pop(1)
+print(thislist)
+
+#########################################################################################
+
+#if we do not specify the index , the pop() method removes the last item.
+
+#Example.
+
+#Removes the last item.
+
+names = ["nikita","chaku","prachi"]
+names.pop()
+print(names)
+
+#####################################################################################33
+
+#The del keyword also removes the specified index.
+
+#Example
+
+thislist = ["apple","banana","cherry"]
+del thislist[0]
+print(thislist)
+
+
