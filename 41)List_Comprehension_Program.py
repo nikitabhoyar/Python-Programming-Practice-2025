d35ab08ff7368cbed5466c2e5948b19e0658e312
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-12 06:47:09 +0530

    list comprehension program

---
+++ 41)List_Comprehension_Program.py
@@ -0,0 +1,52 @@
+
+#List comprehension offers a shorter syntax when you want to create a new list based on the values 
+# of an existing list.
+
+#Example:
+
+#Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
+
+#Without list comprehension you will have to write a for statement with a conditional test inside:
+
+fruits = ["apple","banana","cherry","custurdapple","mango"]
+
+newlist = []
+
+for x in fruits:
+    if 'a' in x :
+        newlist.append(x)
+
+print(newlist)
+
+############################################################################################################
+
+#With list comprehension you can do all that with only one line of code:
+
+#Example
+
+fruits = ["apple","banana","grapes","watermelon","cherry","mango","kiwi","custardapple"]
+
+newlist = [x for x in fruits if 'a']
+
+print(newlist)
+
+#############################################################################################################
+
+#The Syntax
+
+#newlist = [expression for item in iterable if condition == True]
+
+#The return value is a new list, leaving the old list unchanged.
+
+##############################################################################################################
+
+# Condition
+# The condition is like a filter that only accepts the items that evaluate to True.
+
+#Example
+#Only accept items that are not "apple":
+
+newlist = [x for x in fruits if x != "apple"]
+print(newlist)
+
+
