839360c12ad781e082220bc35674593503590177
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-12-15 18:35:22 +0530

    If statement logic program is added

---
+++ 43)If_Statement_Program.py
@@ -0,0 +1,42 @@
+a = 33 
+b = 200
+if b > a :
+    print("b is greater than a")
+
+
+number = 15
+if number > 0:
+    print("Number is positive")
+
+
+
+########################################################################################
+
+
+#Multiple Statement is in If Block
+
+age = 20
+if age >= 18:
+    print("You are eligible")
+    print("You can vote")
+    print("You have full legal rights")
+
+
+##################################################################################
+
+
+#Using Variables in Conditions 
+
+#Boolean variables can be used directly in if statements without comparison operators.
+
+is_logged_in = True
+if is_logged_in : 
+    print("Welcome Back!")
+
+####################################################################################
+
+
+
+
+
+
